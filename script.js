e0444faa2ec8eb705377bac74dacfe0955510f3e
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-06-22 20:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 
 // Focus on the next input when a digit is entered
-function focused(input) 
-{
-    const nextInput = input.nextElementSibling;
-    if (nextInput) 
-	{
-        nextInput.focus();
-    }
-}
- function handleBackspace(event) 
- {
-            if (event.key === 'Backspace') {
-                const currentInput = event.target;
-                const prevInput = currentInput.previousElementSibling;
-                if (prevInput) {
-                    prevInput.focus();
-					currentInput.value = ''; 
-				    currentInput=prevInput;
-					currentInput.focus();
-                }
-				
-            }
+let inputElements = document.getElementsByClassName("code");
+console.log(inputElements.length)
+for(let i=0; i< inputElements.length; i++){
+	inputElements[i].addEventListener("keyup", (event)=>{
+		let currentElement = event.target ;
+		let code = event.key.charCodeAt(0);
+		console.log(event.key)
+		if(event.key === "Backspace"){
+			let prevElement = currentElement.previousElementSibling ;
+			if(prevElement){
+				prevElement.focus();
+			}
+			return ;
+		}
+		
+		
+		
+		if(code>=48 && code<=57){
+			let nextElement = currentElement.nextElementSibling ;
+			if(nextElement){
+				nextElement.focus() ;
+			}
+			
+		}else{
+			event.target.value="" ;
+		}
+	})
+	
 }
